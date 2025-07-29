7c19a194e80ec31e20861cd73083032cda3d8de6
SachitaTiwari <153295289+SachitaTiwari@users.noreply.github.com>
2025-07-29 22:18:07 +0530

    Update NFT-Marketplace.sol

--- contracts/NFT-Marketplace.sol
+++ contracts/NFT-Marketplace.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: 
+// SPDX-License-Identifier: mit
 pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
